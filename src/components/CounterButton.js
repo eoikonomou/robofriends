2dac685cadd9e5338494a3b52010f37ce901cfe4
Stathis Oikonomou <soikonomou@maseurope.com>
2018-08-30 10:26:37 +0300

    adding counter button that only updates when its state is changed to avoid unnecessary re-rendering and creating extra component for the header which does not update since it has no state and no props

---
+++ src/components/CounterButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class CounterButton extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            count: 0
+        };
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.count !== this.state.count;
+    }
+
+    updateCount = () => {
+        this.setState(previousState => {
+            return { count: previousState.count + 1 };
+        });
+    }
+
+    render() {
+        return (
+            <button color={this.props.color} onClick={this.updateCount}>
+                Count: {this.state.count}
+            </button>
+        );
+    }
+}
+
+export default CounterButton;
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import { setSearchField, requestRobots } from '../actions';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import Header from '../components/Header';
 import './App.css';
 
 const mapStateToProps = (state) => {
@@ -39,7 +40,7 @@ class App extends React.Component {
         } else {
             return (
                 <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
+                    <Header />
                     <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <CardList robots={filteredRobots} />
